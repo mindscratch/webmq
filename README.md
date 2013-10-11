bd467f59de66aeb9533fa4f1b0b82f97226f03e3
Craig Wickesser <craig@mindscratch.org>
2013-10-11 16:10:19 -0400

    added grape gem, configured app to autoload api code, added initializer to reload api code in development

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+## Architecture
+
+- built on [rails-api](https://github.com/rails-api/rails-api)
+- uses [grape](https://github.com/intridea/grape)
+- api docs generated using [grape-swagger](https://github.com/tim-vandecasteele/grape-swagger)
--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,9 @@ module Webmq
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.paths.add "app/api", glob: "**/*.rb"
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += Dir["#{Rails.root}/app/api/*"]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
